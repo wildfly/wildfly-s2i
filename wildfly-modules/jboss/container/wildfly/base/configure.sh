2a5322ec6d60a64e5d76d16f35d091219452f80a
JF Denise <jdenise@redhat.com>
2019-06-27 19:16:04 +0200

    rely on wildfly-cekit-modules modules that append to standalone.conf

--- wildfly-modules/jboss/container/wildfly/base/configure.sh
+++ wildfly-modules/jboss/container/wildfly/base/configure.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Configure module
 set -e
+SCRIPT_DIR=$(dirname $0)
+ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
 
 # Create empty JBOSS_HOME and bin dir
 mkdir -p $JBOSS_HOME/bin
+
+# Add standalone.conf, this is transient until galleon allows to handle it.
+cp -r ${ARTIFACTS_DIR}/* ${JBOSS_HOME}
