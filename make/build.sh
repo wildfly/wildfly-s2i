86d3a6ec143072723d2b8a8355aa4f8bdb887d3f
JF Denise <jdenise@redhat.com>
2019-09-10 17:21:00 +0200

    Exit 1 if cekit tests fail

--- make/build.sh
+++ make/build.sh
@@ -10,6 +10,10 @@ popd
 if [[ ! -z "${TEST_MODE:-}" ]]; then
   pushd ${SCRIPT_DIR}/../wildfly-builder-image
   cekit test behave
+  if [ $? -ne 0 ]; then
+    echo "cekit test behave failed."
+    exit 1
+  fi
   popd
   ${SCRIPT_DIR}/../test/run
 fi
