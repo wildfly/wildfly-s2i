03362f9d79f5b95ff944d57c7d974fa54001564a
JF Denise <jdenise@redhat.com>
2020-03-17 10:06:59 +0100

    Enable verbose mode

--- make/build.sh
+++ make/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 SCRIPT_DIR=$(dirname $0)
 pushd ${SCRIPT_DIR}/../wildfly-runtime-image
-cekit build docker
+cekit -v build docker
 popd
 pushd ${SCRIPT_DIR}/../wildfly-builder-image
-cekit build docker
+cekit -v build docker
 popd
 
 if [[ ! -z "${TEST_MODE:-}" ]]; then
