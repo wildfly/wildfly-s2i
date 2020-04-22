1c228de807c9660b80fe5aca000d57983bebaf19
Yeray Borges <yborgess@redhat.com>
2020-04-22 12:41:34 +0100

    WIP

--- wildfly-modules/jboss/container/wildfly/startup/artifacts/bin/openshift-launch.sh
+++ wildfly-modules/jboss/container/wildfly/startup/artifacts/bin/openshift-launch.sh
@@ -7,4 +7,4 @@ PUBLIC_IP_ADDRESS=${WILDFLY_PUBLIC_BIND_ADDRESS:-$(hostname -i)}
 MANAGEMENT_IP_ADDRESS=${WILDFLY_MANAGEMENT_BIND_ADDRESS:-0.0.0.0}
 ENABLE_STATISTICS=${WILDFLY_ENABLE_STATISTICS:-true}
 
-launchServer "$JBOSS_HOME/bin/standalone.sh -c $SERVER_CONFIG -b ${PUBLIC_IP_ADDRESS} -bmanagement ${MANAGEMENT_IP_ADDRESS} -Dwildfly.statistics-enabled=${ENABLE_STATISTICS}"
+launchServer "$JBOSS_HOME/bin/standalone.sh -c $SERVER_CONFIG -b ${PUBLIC_IP_ADDRESS} -bmanagement ${MANAGEMENT_IP_ADDRESS} -Dwildfly.statistics-enabled=${ENABLE_STATISTICS}" "--read-only-server-config=${SERVER_CONFIG}"
