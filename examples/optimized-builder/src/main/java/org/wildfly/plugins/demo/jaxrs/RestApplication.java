02e5157e49a128d5f7607b28dbfc279d73001dd7
JF Denise <jdenise@redhat.com>
2022-10-19 11:40:01 +0200

    Fix for Issue #395, Introduce wildfly-builders projects

---
+++ examples/optimized-builder/src/main/java/org/wildfly/plugins/demo/jaxrs/RestApplication.java
@@ -0,0 +1,8 @@
+package org.wildfly.plugins.demo.jaxrs;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
